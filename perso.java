ed54d980606cfa7a7649b58e4c0e80825cf2c91c
HillelPa <64850372+HillelPa@users.noreply.github.com>
2020-05-06 13:05:19 +0200

    ajout de l'attribut Attaque, ajout des attaque
    System de distance : pour l'instant la distance c'est la somme de la difference des x et des y (pas ouf) mais j'avais la flemme de faire pythagore... on peut garder comme ca je pense

--- perso.java
+++ perso.java
@@ -7,16 +7,22 @@ public class perso{
 	public int type; 				//retrouver chaque perso plus facilement
 	public int posx; 				// la position du joueur sur x 
 	public int posy;				// la position du joueur sur y 
-
+	public Attaque A1;  			// premiere attaque
+	public Attaque A2;				//deuxieme attaque
+	
 	//constructeur
 	public perso(int a, int j){		//a : personnage choisi, j = joueur (1 ou 2)
+		
+		this.A1 = new Attaque(a,true); //attaque a distance
+		this.A2 = new Attaque(a, false); //attaque au corps a corps
+		
 		this.posx = 4;
 		
 		if(j == 1){
-					posy = 0; 		// centre haut du jeu
-				}else{
-					posy = 8;		// centre bas du jeu
-				}
+				posy = 0; 		// centre haut du jeu
+			}else{
+				posy = 8;		// centre bas du jeu
+			}
 		
 		switch(a){
 			case 1 :
@@ -57,9 +63,14 @@ public class perso{
 	
 	//description
 	public String toString(){
-		return this.nom+", vous avez "+this.pv+" points de vie";
+		return this.nom+", vous avez "+this.pv+" points de vie \n"+"Ses attaques sont : "+this.A1.toString()+" et "+this.A2.toString();
 		}
 
+	//description point de vie
+	public String toStringPV(int j){
+		return "PV joueur "+j+" : "+this.pv;
+	}
+	
 	//Déplacements 
 	public void move(){
 		Scanner sc = new Scanner(System.in);
@@ -98,4 +109,41 @@ public class perso{
             this.posx = this.posx-1;
         }
 }
+
+	//Distance
+	public int distance(perso B){
+		return Math.abs(this.posx - B.posx) + Math.abs(this.posy - B.posy);
+	}
+	
+	//perte de point de vie
+	public void pv(int a){
+		this.pv = this.pv - a;
+	}	
+	 
+	//attaque
+public void attaque(perso B){
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Vous décidez d'attaquer ! quelle attaque voulez vous utiliser ?");
+	System.out.println(this.A1.toString()+" tapez 1");
+	System.out.println(this.A2.toString()+" tapez 2");
+	int at = sc.nextInt();
+	int d = this.distance(B);
+	switch (at){
+		case 1 : 
+	if(d > 3){
+		B.pv(this.A1.Adist);
+	}else{
+		B.pv(this.A1.Acac);
+	}
+	break;
+		case 2 : 
+		if(d > 3){
+		B.pv(this.A2.Adist);
+	}else{
+		B.pv(this.A2.Acac);
+	}
+	}
+}
+	
 }
