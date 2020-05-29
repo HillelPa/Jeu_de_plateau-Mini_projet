fd8da668a2e88ae0a6054ad344c9dd525096a3c3
rbrun74 <65080467+rbrun74@users.noreply.github.com>
2020-05-29 14:35:28 +0200

    Update Attaque.java

--- Attaque.java
+++ Attaque.java
@@ -20,7 +20,7 @@ public class Attaque{
 		break;
 		case 2 :
 			if(d == true){
-				this.nom = "lancé de couteaux";
+				this.nom = "lancer de couteaux";
 				this.Adist = 10;
 				this.Acac = 5;
 			}else{
